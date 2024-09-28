1e50c42d379f9669986dda37f69da0a24167c578
CoomSalad <chanlokfung816@gmail.com>
2024-09-28 10:26:04 +0800

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import "./style.css";
-import { initPage, clearPage } from "./initpage.js";
-import { renderHome } from "./homepage.js";
-import { renderMenu } from "./menupage.js";
-import { renderContact } from "./contactpage.js";
+import { DisplayController } from "./displayController.js";
 
-const renderPage = (renderer) => {
-    clearPage();
-    renderer();
+function initPage() {
+    DisplayController.init();
 }
-
-const homeBtn = document.querySelector(".home-btn");
-homeBtn.addEventListener("click", function() { renderPage(renderHome); });
-const menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click", function() { renderPage(renderMenu); });
-const contactBtn = document.querySelector(".contact-btn");
-contactBtn.addEventListener("click", function() { renderPage(renderContact); });
 
 initPage();
---
+++ src/task.js
@@ -0,0 +1,7 @@
+const createTask = (title_param) => {
+    const title = title_param;
+  
+    return { title };
+}
+
+export { createTask };
